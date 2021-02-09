35e705f54494d7b6f581b034c7f2a4ad18b91e32
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-02-09 13:25:01 +0800

    this is practice

--- index.php
+++ index.php
@@ -5,77 +5,128 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>practicePHP</title>
+    <!-- w3-css -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- JavaScript Bundle with Pogit pper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
+ <div class="container">
+    <div class="row">
+        <div class= "col-sm-3">
+          <form action="index.php" method="post">
 
-<?php
+              <h1>Registration</h1>
+              <p>FIll up all the forms with correct values</p>
+              <hr class="mb-3">
 
-$fname = $_POST['firstname'];
-$lname = $_POST['lastname'];
-$email = $_POST['email'];
-$pnumber = $_POST['phonenumber'];
-$pass = $_POST['password'];
+              <label><b>First Name</b></label>
+              <input type="firstname" class="form-control" name="firstname" required>
 
-echo $firstname . " " . $lastname . " " .$email . " " . $phonenumber . " " . $password ;
+              <label><b>Last Name</b></label>
+              <input type="lastname" class="form-control" name = "lastname" required>
 
-$servername = "localhost";
-$username = "root";
-$password = " ";
-$dbname = "users";
+              <label><b>Email Address</b></label>
+              <input type="email" class="form-control" name = "email" required>
 
+              <label><b>Phonenumber</b></label>
+              <input type="phonenumber" class="form-control" name = "phonenumber" required>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+              <label><b>Password</b></label>
+              <input type="password" class="form-control" name = "password" required>
+              
+              <hr class="mb-3">
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+              <button type="submit" class="btn btn-primary" value="submit">Submit</button>
+          </form>
+       </div>
+    </div>
+ </div>
 
-$sql = "INSERT INTO userprofile (firstname, lastname, email, phonenumber, password)
-VALUES ($fname,$lname, $email,$pnumber,$pass)";
+    <?php
+    
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $pnumber = $_POST['phonenumber'];
+    $pass = $_POST['password'];
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "useraccounts";
 
-$conn->close();
+    //conditions where phonenumber validation
+    if(isset($_POST['phonenumber'])){
+      if (is_numeric($pnumber)) {
+        echo 'The number you entered is ' . $pnumber. '. This is a valid number.';
+        }
+        else {
+        echo 'Error: You did not enter numbers only. Please enter only numbers.';
+        }
+    }
 
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-?>
-<form  action="registration.php" method = "post">
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    
+    //insert data to database
+    $sql = "INSERT INTO users(firstname, lastname, email, phonenumber, user_pass)
+    VALUES ('$fname', '$lname' , '$email', '$pnumber','$pass')";
+
+    // sql to delete a record
+    $sql = "DELETE FROM users WHERE id=1";
+
+    if (mysqli_query($conn, $sql)) {
+      echo "Record deleted successfully";
+    } else {
+      echo "Error deleting record: " . mysqli_error($conn);
+    }
+
+    //check if data insert to database successfully
+    if ($conn->query($sql) === TRUE) {
+      echo "<br> New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
+    ?>
+
+<!-- <form  action="registration.php" method = "post">
     <div class="container">
                 <div class="row">
                         <div class="col-sm-3">
                                 <h1>Registration</h1>
                                 <p>FIll up all the forms with correct values</p>
                                 <hr class="mb-3">
-                                <label for="firstname"><b>First Name</b></label>
+                                <label><b>First Name</b></label>
                                 <input type="text" class="form-control" name="firstname" id="register" required>
 
-                                <label for="lastname"><b>Last Name</b></label>
+                                <label><b>Last Name</b></label>
                                 <input type="text" class="form-control" name="lastname"  id="register" required>
 
-                                <label for="email"><b>Email</b></label>
+                                <label><b>Email</b></label>
                                 <input type="email" class="form-control" name="email"  id="register" required>
 
-                                <label for="phonenumber"><b>Phone Number</b></label>
+                                <label><b>Phone Number</b></label>
                                 <input type="text" class="form-control" name="phonenumber"  id="register" required>
 
-                                <label for="password"><b>Password</b></label>
+                                <label><b>Password</b></label>
                                 <input type="password" class="form-control" name="password"  id="register"required>
                                 <hr class="mb-3">                           
-                                <input type="submit" class="btn btn-primary" class="form-control" name ="create" id="register"  value="Sign Up">
+                                <input type="submit" class="btn btn-primary" class="form-control" id="register"  value="Sign Up">
                         </div>
                 </div>
      </div>
- </form>
+ </form> -->
 
 </body>
 </html>
